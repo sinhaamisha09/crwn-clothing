db958a6b8a3b4261bb6b7fdb74b436865863dba6
sinhaamisha09 <sinhaamisha09@gmail.com>
2019-09-30 10:36:16 +0530

    broken our homepage into smaller components, finished styling our menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '/home/amisha/Desktop/React-new/crwn-clothing/src/components/directory/directory,component.jsx';
+import './homepage.styles.scss';
+ 
+const HomePage = () =>  (
+	<div className='homepage'>
+    	<Directory/>
+	 </div>
+);
+
+export default HomePage;
