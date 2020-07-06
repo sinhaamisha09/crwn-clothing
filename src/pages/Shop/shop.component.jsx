1e74babbe3538256e98ddd059bfbfbb8de5303a1
Amisha Sinha <36271876+sinhaamisha09@users.noreply.github.com>
2020-07-06 19:07:01 +0530

    Update shop.component.jsx

--- src/pages/Shop/shop.component.jsx
+++ src/pages/Shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '/home/amisha/Desktop/React-new/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
  
 class ShopPage extends React.Component {
 	constructor(props) {
